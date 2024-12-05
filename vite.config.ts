570172e89f30f180f6cdbaf04d56d3f46bc2252f
MagicInUse <jacob.norr@live.com>
2024-12-04 21:09:35 -0700

    vite package build changes

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://api.github.com',
+        target: process.env.VITE_BASE_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
