c16829c543bef70c7512f9b434ab62ee51439492
MagicInUse <jacob.norr@live.com>
2024-12-04 21:04:35 -0700

    vite build config settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envDir: '.', // Change this line to point to the root directory
-  envPrefix: 'VITE_', // Add this line
+  envDir: '.',
+  envPrefix: 'VITE_',
   plugins: [react()],
   server: {
     proxy: {
@@ -12,6 +12,16 @@ export default defineConfig({
         target: 'https://api.github.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+        },
       },
     },
   },
